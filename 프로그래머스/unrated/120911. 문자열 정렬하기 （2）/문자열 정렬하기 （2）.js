4bd288aee3166547f783b43c4362abd0f154dcfb
eunjoo0311 <120389195+eunjoo0311@users.noreply.github.com>
2023-11-06 17:34:38 +0900

    [level unrated] Title: 문자열 정렬하기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/unrated/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,6 +1,5 @@
 function solution(my_string) {
     var answer = '';
-    let arr = [...my_string.toLowerCase()];
-    answer = arr.sort().join('')
+    answer = [...my_string.toLowerCase()].sort().join('')
     return answer;
 }
