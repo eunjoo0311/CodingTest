973d0cb19682ad797a28cc745fd18e2d262d187e
eunjoo0311 <120389195+eunjoo0311@users.noreply.github.com>
2023-11-07 14:05:46 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,9 +1,5 @@
 function solution(num) {
     var answer = '';
-    if(num % 2 ==0){
-        answer = "Even"
-    } else {
-        answer = "Odd"
-    }
+    answer = num % 2 ==0 ? 'Even' : 'Odd'
     return answer;
 }
