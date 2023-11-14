301af687f39b67545f03256e1226b43318734355
eunjoo0311 <120389195+eunjoo0311@users.noreply.github.com>
2023-11-14 23:10:11 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,9 @@
+function solution(my_string, num1, num2) {
+    var answer = '';
+    let arr = [...my_string];
+    let a = arr[num1];
+	arr[num1] = arr[num2];
+    arr[num2] = a;
+    answer = arr.join('')
+    return answer;
+}
