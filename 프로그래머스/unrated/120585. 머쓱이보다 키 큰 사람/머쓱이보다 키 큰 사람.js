89a9650bcef87bc07d74697e22406a7bec72a08f
eunjoo0311 <120389195+eunjoo0311@users.noreply.github.com>
2023-11-24 23:56:45 +0900

    [level unrated] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -0,0 +1,12 @@
+function solution(array, height) {
+    var answer = 0;
+    let a = 0;
+    for(let i = 0; i<array.length; i++){
+        if(array[i]>height){
+            a++;
+        }
+    answer = a
+        
+    }
+    return answer;
+}
